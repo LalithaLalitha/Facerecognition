4f877a8c12c20f41f5a92e680516ded2568a3a16
LalithaLalitha <99866646+LalithaLalitha@users.noreply.github.com>
2022-04-03 12:31:51 +0530

    Update face_detector.py

--- AIProject/face_detector.py
+++ AIProject/face_detector.py
@@ -5,7 +5,7 @@ import cv2
 trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #choose an image to detext face
-img = cv2.imread("RDJ2.jpeg")
+img = cv2.imread("RDJ2.jpg")
 
 #covert it to grey scale
 grayscaled_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
